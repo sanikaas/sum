8f40fc6f3a09c4869d0e82eef9b9254d71c1ba00
sanikaas <sanikaas13@gmail.com>
2025-10-31 10:09:32 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-a=float(input("Enter first number:"))
-b=float(input("Enter the second number:"))
+a=float("Enter first number:")
+b=float("Enter the second number:")
 add=a+b
 print(f"The sum of {a} and {b} is {add}")
